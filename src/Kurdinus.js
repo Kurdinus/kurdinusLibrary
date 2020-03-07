e65e9088ef3fe97f181333a02cf0b8629487b7ad
Siamand <me@siamand.cc>
2020-03-07 15:30:51 +0100

    feat: add changeCase method to Kurdinus

--- src/Kurdinus.js
+++ src/Kurdinus.js
@@ -1,4 +1,10 @@
 import * as Constants from './constants';
 import { CaseType, FontType, NumerlaType, PhonemeType } from './enums';
 
+import * as Latin from './converters/latin';
+
+export function changeCase(text, caseType){
+	return new Latin.CaseConverter(caseType).convert(text)
+}
+
 export { CaseType, FontType, NumerlaType, PhonemeType };
