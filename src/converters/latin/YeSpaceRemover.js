4eeb00a5f1716f2700a57b85905c64a948883a05
Siamand <me@siamand.cc>
2020-03-08 23:46:25 +0100

    feat: add Latin YeSpaceRemover

---
+++ src/converters/latin/YeSpaceRemover.js
@@ -0,0 +1,18 @@
+import RegexConverter from '@/converters/RegexConverter.js'
+import {
+  KURMANCI_SUFFIXES,
+  LATIN_LETTERS,
+  SUFFIX_MERGE_DIVIDER
+} from '@/constants'
+
+export default class YeSpaceRemover extends RegexConverter {
+    constructor(){
+        super();
+        this.conversions = [
+            {
+                regex : '([^' + LATIN_LETTERS + '])(' + KURMANCI_SUFFIXES + ')($|[^' + LATIN_LETTERS + '])',
+                to: '$1' + SUFFIX_MERGE_DIVIDER + '$2$3'
+            }
+        ]
+    }
+}
--- src/converters/latin/index.js
+++ src/converters/latin/index.js
@@ -1,7 +1,9 @@
 import CaseConverter from './CaseConverter.js'
 import DiacriticalConverter from './DiacriticalConverter.js';
+import YeSpaceRemover from './YeSpaceRemover.js';
 
 export {
   CaseConverter,
-  DiacriticalConverter
+  DiacriticalConverter,
+  YeSpaceRemover
 }
