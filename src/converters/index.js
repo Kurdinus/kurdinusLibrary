793f8c4360d266b3100f2601ecda3d2fea2e15b8
Aso Mahmudi <aso.mehmudi@gmail.com>
2020-03-29 09:32:02 +0430

    PresentationFormsNormalizer
    Arabic Presentation Forms are deprecated

--- src/converters/index.js
+++ src/converters/index.js
@@ -1,7 +1,9 @@
+import PresentationFormsNormalizer from './PresentationFormsNormalizer.js';
 import PunctuationNormalizer from './PunctuationNormalizer.js';
 import NumberConverter from './NumberConverter.js';
 
 export {
+  PresentationFormsNormalizer,
   PunctuationNormalizer,
   NumberConverter
 }
